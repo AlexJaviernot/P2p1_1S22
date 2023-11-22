4cf6025db24a3d0be8609198375bd7ecb8460c9b
AlexJaviernot <144191141+AlexJaviernot@users.noreply.github.com>
2023-11-21 19:46:12 -0600

    Add files via upload

--- P3p6_1S22.java
+++ P3p6_1S22.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
-
- class p3p6_1s22{
-
-public static int promedio(int a, int b, int c){
-return((a+b+c)/3);
-}
-
-public static int[] captura(){
-int[] numeros = new int[10];
-Scanner teclado = new Scanner(System.in);
-for (int a=0;a<10;a++) {
-System.out.println("ingrese el valor para la posicion ["+(a+1)+": ");
-numeros[a]=teclado.nextInt();
-}
-return numeros;
-}
-
-    public static void main(String[] args) {
-    int[] valores = captura();
-    for (int x = 0;x<valores.length;x++){
-    System.out.println("valores contiene en la posicion["+(x+1)+"]: "+valores[x]);
-    }
-    System.out.println("el promedio de los 3 numeros es " + promedio(8,5,9));
-
-    }
-}
-
+class P3P6_1S22{
+	public static int promedio(int[] a){
+		int sum=0;
+		for (int num : a)
+		sum+=num;
+		sum=sum/(a.length);
+		return sum;
+	}
+	public static int[] captura(){
+		Scanner teclado = new Scanner(System.in);
+		int[] numeros = new int[10;
+		for (int x=0;x<10;x++){
+			System.out.println("valores que tiene en la posicion ["+(a+1)+"]: ");   
+			numeros[x]=teclado.nextInt();                                                  
+		}
+		return numeros;
+		}
+		public static void main(String[] args) {
+			int[] arreglo = captura();
+			System.out.println("El promedio de los numero es " +promedio(arreglo));
+		}
