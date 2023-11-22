22fbf2377561ca2cceb59b70f77deb017998966a
AlexJaviernot <144191141+AlexJaviernot@users.noreply.github.com>
2023-11-21 18:12:56 -0600

    Add files via upload

---
+++ P2P3_IS22.java
@@ -0,0 +1,23 @@
+public class P2P3_1S22{
+	public static void main(String[] args){
+		int a=0;
+
+		while(a<11){
+		     System.out.println("el valor de a vale " +a);
+		a++;
+		}
+	a=0;
+	boolean estado = true;
+	while(estado){
+    System.out.println("Nuevamente a vale "+a);
+    if (a>9) break;
+    a++;
+	}
+   estado = false;
+
+   while(estado){
+   System.out.println("se mostrara la linea?");
+   
+      }
+   }
+}
---
+++ P2P4_IS22.java
@@ -0,0 +1,19 @@
+public class P2P4_IS22{ 
+  public static void main(String[] args){
+  	int a=0;
+  	do{
+  		System.out.println("El valor de a es de " + a);
+  		a++;
+  	}while(a<11);
+   
+  a=0;
+  do{
+  	System.out.println("Este es el nuevo valor de a "+a);
+  	a++;
+  	if(a>10) break;
+    }while(true);
+  do{
+  	System.out.println("Se mostrara esta linea?");
+    }while(false);
+  }
+}
